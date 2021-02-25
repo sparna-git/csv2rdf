acdec799364e70641e9dfb84db684f3206c0a2a8
Thomas Francart <thomas.francart@sparna.fr>
2021-02-25 16:15:34 +0100

    comments

--- src/main/java/fr/sparna/rdf/csv2rdf/CSV2RDF.java
+++ src/main/java/fr/sparna/rdf/csv2rdf/CSV2RDF.java
@@ -28,10 +28,14 @@ public class CSV2RDF {
 	}
 	
 	public Model convert(InputStream in) throws IOException {
+		// Create empty Model
 		Model tableModel = ModelFactory.createDefaultModel();
 		
+		// init parser, we suppose the first row contains a header
 		CSVParser parser = CSVParser.parse(new InputStreamReader(in), CSVFormat.DEFAULT.withFirstRecordAsHeader());
+		// get the header names
 		List<String> headers = parser.getHeaderNames();
+		// create properties from header
 		List<Property> properties = headers.stream().map(s -> {
 			try {
 				return tableModel.createProperty( this.base + URLEncoder.encode(s.replace(" ", "_"), "UTF-8"));
@@ -40,11 +44,17 @@ public class CSV2RDF {
 				return null;
 			}
 		}).collect(Collectors.toList());
+		
+		// iterate on all CSV Records
 		for (CSVRecord record : parser.getRecords()) {
+			// create new anonymous resource for each line
 			Resource subject = tableModel.createResource();
 			subjectCount++;
+			// iterate on each column
 			for(int i=0; i < headers.size(); i++) {
-				if(record.isSet(i)) {
+				// if record has this column set
+				if(record.isSet(i) && !record.get(i).equals("")) {
+					// add a new property on this subject
 					subject.addProperty(properties.get(i), record.get(i));
 					tripleCount++;
 				}
@@ -63,10 +73,11 @@ public class CSV2RDF {
 	}
 
 	public static void main(String...strings) throws Exception {
-		String input = "id,first name,last name"+"\n"+"1,Thomas,Francart"+"\n"+"2,Jorge,Aquino";
+		String input = "id,first name,last name"+"\n"+"1,Thomas,Francart"+"\n"+"2,Jorge,Aquino"+"\n"+"3,,Test";
 		CSV2RDF csv2rdf = new CSV2RDF("http://sparna.fr/");
 		InputStream in = new ByteArrayInputStream(input.getBytes());
 		Model m = csv2rdf.convert(in);
+		System.out.println("Produced "+csv2rdf.getSubjectCount()+" resources and "+csv2rdf.getTripleCount()+" triples");
 		m.write(System.out, "Turtle");
 	}
 	
